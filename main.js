e136bc38edcd9426269474512051bbf0a28e130f
gavmac33 <gavin.m.macpherson@vanderbilt.edu>
2019-07-27 15:49:15 -0400

    Removed rasterize

--- main.js
+++ main.js
@@ -2,15 +2,9 @@
 const linkClass = "iUh30";
 
 
-document.body.appendChild(document.createElement('script')).src = 'node_modules/rasterizehtml/dist/rasterizeHTML.allinone.js';
-
-
 function main() {
     let pageLinks = document.body.getElementsByClassName(linkClass);
 
-    //let temp = rasterizeHTML.drawURL(pageLinks[0].innerHTML);
-
-    // pageLinks[0].insertAdjacentElement('beforebegin', temp)
     
     for(let page of pageLinks){
         let iframe = document.createElement("iframe");
